d4dcfc1d3b17dc017178ded27891afa01120bc74
Iftach Naftaly <iftahnaf@gmail.com>
2023-11-04 09:51:31 +0000

    refactor(examples): adding plot to the bounded interception

--- examples/bounded_interception.py
+++ examples/bounded_interception.py
@@ -1,21 +1,78 @@
 import pyvectorguidance
 import numpy as np
+from dynamics import SixDOFDroneDynamics
+import matplotlib.pyplot as plt
+import pylab
 
 def main():
+    drone = SixDOFDroneDynamics()
+    target = SixDOFDroneDynamics()
+
     vg = pyvectorguidance.VectorGuidance()
 
-    r = np.random.rand(3) * np.random.uniform(40, 60, size=1)
-    v = np.random.rand(3) * np.random.uniform(5, 15, size=1)
+    drone.position = np.ones(3,) * np.random.uniform(0, 20, size=1)
+    drone.velocity = np.ones(3,) * np.random.uniform(0, 15, size=1)
+
+    target.position = np.ones(3,) * np.random.uniform(100, 200, size=1)
+    target.velocity = np.ones(3,) * np.random.uniform(0, 5, size=1)
+
+    initial_drone_position = drone.position.copy() - [50, 50, 50]
+    initial_target_position = target.position.copy() + [50, 50, 50]
 
     rho_w = 9.81
     rho_u = 15.0
     gz = 9.81
+    w = np.array([0.0, 0.0, 9.81]) # non maneuvering target
 
-    tgo = vg.interception_tgo_bounded(r, v, rho_u, rho_w)[0]
-    u = vg.interception_controller_bounded(r, v, rho_u, tgo, gz)
+    tgo = 100.0
 
-    print(tgo, u)
+    i = 0
 
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(111, projection='3d')
+
+    tmp_dist = 1000.0
+    dist = np.linalg.norm(target.position - drone.position)
+    
+    while tgo > 0.01:
+
+        tmp_dist = dist
+
+        r = target.position - drone.position
+        v = target.velocity - drone.velocity
+
+        tgo = vg.interception_tgo_bounded(r, v, rho_u, rho_w)
+        u = vg.interception_controller_bounded(r, v, rho_u, tgo, gz)
+
+        drone.step(u)
+        target.step(w)
+
+        dist = np.linalg.norm(target.position - drone.position)
+
+        if i > 1000 and tmp_dist > dist:
+            break
+
+        ax.scatter(drone.position[0], drone.position[1], drone.position[2], label='Drone', c='b')
+        ax.scatter(target.position[0], target.position[1], target.position[2], label='Drone', c='r')
+
+        ax.set_title(f"Miss Distance = {dist:.5} [m]\nTgo = {tgo:.6} [sec]\nTotal Scenario Time = {(i  * drone.time_step):.4} [sec]")
+
+        ax.text(drone.position[0] - 25, drone.position[1] - 25, drone.position[2] - 10, "Drone")
+        ax.text(target.position[0] + 25, target.position[1] + 25, target.position[2] + 10, "Target")
+        
+        ax.set_xlim(initial_drone_position[0], initial_target_position[0])
+        ax.set_ylim(initial_drone_position[1], initial_target_position[1])
+        ax.set_zlim(initial_drone_position[2], initial_target_position[2])
+
+        plt.pause(0.0001)
+
+        if i % 2 == 0:
+            ax.cla()
+            
+        i += 1
+    print(f"Final Miss Distance = {dist:.5} [m], Total Scenario Time = {i  * drone.time_step} [sec]")
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
---
+++ examples/dynamics.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class SixDOFDroneDynamics:
+    def __init__(self, time_step: float=0.01):
+        self.time_step = time_step
+
+        self.gravity = np.array([0.0, 0.0, 9.81])
+        self.position = np.array([0.0, 0.0, 100.0])
+        self.velocity = np.array([0.0, 0.0, 0.0])
+
+    def step(self, u):
+        a = u - self.gravity
+        self.velocity += a * self.time_step
+        self.position += self.velocity * self.time_step
+
+
+def main():
+    drone = SixDOFDroneDynamics()
+    for i in range(1000):
+        drone.step(np.array([0, 0, 0]))
+        print(drone.position)
+
+if "__main__" == __name__:
+    main()
