3c272f675db6a6d879bffb47e58891e6223c0377
Iftach Naftaly <58637596+iftahnaf@users.noreply.github.com>
2023-11-17 15:27:12 +0200

    recator: project's ci (#36)

---
+++ pyvectorguidance_setup.py
@@ -0,0 +1,8 @@
+__title__ = 'pyvectorguidance'
+__description__ = ('pyvectorguidance')
+__url__ = 'https://github.com/iftahnaf/pyvectorguidance'
+__version__ = '1.0.3'
+__author__ = 'Iftach Naftaly'
+__author_email__ = 'iftahnaf@gmail.com'
+__license__ = 'LICENSE'
+__copyright__ = 'Copyright 2023 Iftach Naftaly'
--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import find_packages, setup
+import pyvectorguidance_setup
+
+try:
+    import sys
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -6,15 +14,17 @@ with open("README.md", "r") as fh:
 setup(
   name = 'pyvectorguidance',
   packages=find_packages(),
-  license='Apache Software License',
-  description = 'Vector Guidance methods implemented in Python.',
+  license=pyvectorguidance_setup.__license__,
+  description = pyvectorguidance_setup.__description__,
   long_description=long_description,
   long_description_content_type="text/markdown",
-  author = 'Iftach Naftaly',
-  author_email = 'iftahnaf@gmail.com',
-  url = 'https://github.com/iftahnaf/pyvectorguidance',
+  author = pyvectorguidance_setup.__name__,
+  author_email = pyvectorguidance_setup.__author_email__,
+  url = pyvectorguidance_setup.__url__,
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
   keywords = ['Python', 'Vector Guidance'],
+  version=pyvectorguidance_setup.__version__,
+  title=pyvectorguidance_setup.__title__,
   install_requires=[
           'numpy==1.26.0',
           'rich==13.3.1',
